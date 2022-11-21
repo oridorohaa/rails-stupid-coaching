1b657282bb6b7e9768fe006a377fa6b5b7c4dd0f
Andriana Oridoroha <oridorohaa@gmail.com>
2022-11-21 11:49:07 -0600

    stupid coaching done

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+
+
+  def ask
+
+  end
+
+  def answer
+    @question = params[:question] if params[:question]
+    @nswer = 'not working'
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"]
+    if @question == "I am going to work"
+      @answer = @answers[0]
+    elsif @question.include?('?')
+      @answer = @answers[1]
+    else
+      @answer = @answers[2]
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  root to: 'pages#home'
   # root "articles#index"
+  get "/ask", to: "pages#ask"
+
+  get "/answer", to: "pages#answer"
 end
